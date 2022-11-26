699268d3c1488bd625b632cd4cb3bbc0ddf6e41b
Hoangvu75 <uchihavuhuyhoang@gmail.com>
2022-11-26 21:27:02 +0700

    update

--- hackathon_project/lib/API services/api_service.dart
+++ hackathon_project/lib/API services/api_service.dart
@@ -12,7 +12,9 @@ import 'package:retrofit/http.dart';
 import 'API models/create user/create_user_request.dart';
 import 'API models/create user/create_user_response.dart';
 import 'API models/get campaign list/get_campaign_list_response.dart';
-import 'API models/idea/idea_response.dart';
+import 'API models/idea/create_idea_request.dart';
+import 'API models/idea/create_idea_response.dart';
+import 'API models/idea/get_idea_response.dart';
 import 'API models/login/login_request.dart';
 import 'API models/login/login_response.dart';
 import 'API models/register/register_request.dart';
@@ -59,4 +61,7 @@ abstract class ApiService {
 
   @GET("/idea/get_idea")
   Future<IdeaResponse> getIdeaList();
+
+  @POST("/idea/post_idea")
+  Future<CreateIdeaResponse> postIdea(@Body() CreateIdeaRequest createIdea);
 }
--- hackathon_project/lib/API services/api_service.g.dart
+++ hackathon_project/lib/API services/api_service.g.dart
@@ -138,6 +138,30 @@ class _ApiService implements ApiService {
     return value;
   }
 
+  @override
+  Future<CreateIdeaResponse> postIdea(createIdea) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(createIdea.toJson());
+    final _result = await _dio
+        .fetch<Map<String, dynamic>>(_setStreamType<CreateIdeaResponse>(Options(
+      method: 'POST',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/idea/post_idea',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = CreateIdeaResponse.fromJson(_result.data!);
+    return value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
