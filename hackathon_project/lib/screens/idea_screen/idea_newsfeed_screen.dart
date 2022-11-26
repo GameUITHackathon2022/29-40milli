5d442b3915571b37eeba9d2eba48adc05fbdb0c2
Hieu Ngan <21520358@gm.uit.edu.vn>
2022-11-26 21:33:10 +0700

    temp

--- hackathon_project/lib/screens/idea_screen/idea_newsfeed_screen.dart
+++ hackathon_project/lib/screens/idea_screen/idea_newsfeed_screen.dart
@@ -1,27 +1,57 @@
 import 'dart:ffi';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:hackathon_project/API%20services/API%20models/idea/idea_response.dart';
+import 'package:hackathon_project/API%20services/api_service.dart';
 import 'package:hackathon_project/screens/idea_screen/components/button.dart';
 import 'package:hackathon_project/screens/idea_screen/components/idea_widget.dart';
 import 'package:hackathon_project/screens/idea_screen/new_idea_screen.dart';
 import 'package:hackathon_project/screens/idea_screen/user_ideas_screen.dart';
 
-class IdeaNewsfeedScreen extends StatelessWidget {
+class IdeaNewsfeedScreen extends StatefulWidget {
   IdeaNewsfeedScreen({super.key});
-  var ideaList = [
-    {
-      'avtUrl': 'https://picsum.photos/id/237/200/300',
-      "title": "Title",
-      'content': "Content",
-      'isFavorite': false,
-    },
-    {
-      'avtUrl': 'https://picsum.photos/id/237/200/300',
-      "title": "Title",
-      'content': "Content",
-      'isFavorite': false,
-    },
-  ];
+
+  @override
+  State<IdeaNewsfeedScreen> createState() => _IdeaNewsfeedScreenState();
+}
+
+class _IdeaNewsfeedScreenState extends State<IdeaNewsfeedScreen> {
+  List<Data> ideaList = [];
+
+  Future<void> getIdea() async {
+    EasyLoading.show(maskType: EasyLoadingMaskType.black);
+    try {
+      await ApiService.create().getIdeaList().then((dataItem) {
+        bool? success = dataItem.success;
+        if (success == true) {
+          ideaList = dataItem.data!;
+        }
+      });
+    } catch (obj) {
+      print("${obj}");
+      switch (obj.runtimeType) {
+        case DioError:
+          // Here's the sample to get the failed response error code and message
+          final res = (obj as DioError).response;
+          print(res!.statusCode);
+          break;
+        default:
+      }
+    } finally {
+      EasyLoading.dismiss();
+    }
+  }
+
+  @override
+  void initState() {
+    getIdea();
+    setState(() {}); // TODO: implement initState
+    super.initState();
+  }
+
+  // var ideaList = [
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,10 +73,10 @@ class IdeaNewsfeedScreen extends StatelessWidget {
                 itemBuilder: ((context, index) => Container(
                       margin: EdgeInsets.all(15),
                       child: IdeaWidget(
-                          avtUrl: ideaList[index]["avtUrl"].toString(),
-                          title: ideaList[index]["title"] as String,
-                          content: ideaList[index]["content"] as String,
-                          isFavorite: ideaList[index]["isFavorite"] as bool),
+                          avtUrl: ideaList[index].userImage.toString(),
+                          title: ideaList[index].title as String,
+                          content: ideaList[index].description as String,
+                          isFavorite: false),
                     ))),
           ),
         ],
