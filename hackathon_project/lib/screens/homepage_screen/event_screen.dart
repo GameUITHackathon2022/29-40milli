1311bc761442d79220691b77fceb1c00f78ab8be
minhphan46 <21522345@gm.uit.edu.vn>
2022-11-27 01:53:20 +0700

    update event screen

--- hackathon_project/lib/screens/homepage_screen/event_screen.dart
+++ hackathon_project/lib/screens/homepage_screen/event_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import '../../../API services/API models/get campaign list/get_campaign_list_response.dart';
 
@@ -15,66 +16,88 @@ class EventScreen extends StatelessWidget {
     var date =
         DateTime.fromMillisecondsSinceEpoch(campaignData.startTime as int);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(campaignData.title as String),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 300,
-              child: Image.network(
-                campaignData.image as String,
-                fit: BoxFit.cover,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 25),
-              child: Column(children: [
-                SizedBox(height: 10),
-                Text(
-                  campaignData.title as String,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
+                alignment: Alignment.topRight,
+                children: [
+                  Container(
+                    height: 300,
+                    width: MediaQuery.of(context).size.width,
+                    child: Image.network(
+                      campaignData.image as String,
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ),
-                SizedBox(height: 30),
-                Row(
-                  children: [
-                    Text(
-                      "Date:  " + DateFormat.yMMMd().format(date).toString(),
-                      style: inFoTextstyle,
+                  Padding(
+                    padding: const EdgeInsets.only(top: 28, right: 15),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: Colors.black45),
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.close,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
                     ),
-                  ],
-                ),
-                SizedBox(height: 10),
-                Row(
-                  children: [
-                    Text(
-                      "Location:  " + (campaignData.location as String),
-                      style: inFoTextstyle,
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                child: Column(children: [
+                  SizedBox(height: 10),
+                  Text(
+                    campaignData.title as String,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
                     ),
-                  ],
-                ),
-                SizedBox(height: 10),
-                Row(
-                  children: [
-                    Text(
-                      "Num of member:  " +
-                          campaignData.followers!.length.toString(),
-                      style: inFoTextstyle,
-                    ),
-                  ],
-                ),
-                SizedBox(height: 30),
-                Text(
-                  campaignData.description as String,
-                  style: inFoTextstyle,
-                ),
-              ]),
-            )
-          ],
+                  ),
+                  SizedBox(height: 30),
+                  Row(
+                    children: [
+                      Text(
+                        "Date:  " + DateFormat.yMMMd().format(date).toString(),
+                        style: inFoTextstyle,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Text(
+                        "Location:  " + (campaignData.location as String),
+                        style: inFoTextstyle,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Text(
+                        "Num of member:  " +
+                            campaignData.followers!.length.toString(),
+                        style: inFoTextstyle,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 30),
+                  Text(
+                    campaignData.description as String,
+                    style: inFoTextstyle,
+                  ),
+                ]),
+              )
+            ],
+          ),
         ),
       ),
     );
