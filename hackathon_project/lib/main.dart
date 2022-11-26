e13487a0c5728bf324a3d463e07d2c4a60e1e78a
Hoangvu75 <uchihavuhuyhoang@gmail.com>
2022-11-27 00:29:02 +0700

    Update main.dart

--- hackathon_project/lib/main.dart
+++ hackathon_project/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.green),
-      home: LoginScreen(),
+      home: MainScreen(),
       builder: EasyLoading.init(),
     );
   }
